67667b5a48e25b8880b51626b445b96d3ecc9147
recursioner <recursioner@gmail.com>
2018-03-04 18:27:42 +1100

    add schema so inputs can be verified

---
+++ schema/schema.js
@@ -0,0 +1,20 @@
+var Joi = require('joi');
+module.exports = {
+    sendmail: Joi.object().keys({
+        sender: Joi.string().email(),
+        recipients: Joi.array().items(
+            Joi.string().email()
+        ).required(),
+
+        carboncopys: Joi.array().items(
+            Joi.string().email()
+        ).optional(),
+
+        blindcarboncopys: Joi.array().items(
+            Joi.string().email()
+        ).optional(),
+
+        subject: Joi.string().required(),
+        text: Joi.string().optional()
+    }),
+};
